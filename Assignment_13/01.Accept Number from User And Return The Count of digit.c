8e7c4004ad33263d21832d99517104e504a65142
Gaurav <gauravKatwate7891@gmail.com>
2021-12-15 17:58:34 +0530

    Logic Building in c

---
+++ Assignment_13/01.Accept Number from User And Return The Count of digit.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<conio.h>
+
+int EvenCount(int no)
+{
+   int i = 0, digit = 0;
+
+   while(no != 0)
+   {
+       digit = no % 10;
+       if(digit % 2 == 0)
+       {
+           i++;
+       }
+       no = no / 10;
+   }
+    return i;
+}
+
+int main()
+{
+    int ino = 0;
+
+    printf("\n Enter Any Number => ");
+    scanf("%d",&ino);
+
+    printf("\n Even Digits Count Is %d",EvenCount(ino));
+
+    getch();
+    return 0;
+}
