341afc9e0d69339fceef8b7bd19a2ebe408a646d
Gaurav <gauravKatwate7891@gmail.com>
2021-12-17 21:59:09 +0530

    Logic Building in c

---
+++ Assignment_14/02.Accept Number From User And Display Below Pattern.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+
+void Display(int no)
+{
+    while(no != 0)
+    {
+        printf("  %d  #",no);
+        no--;
+    }
+}
+
+int main()
+{
+    int ino = 0;
+
+    printf("\n Enter Any Number =>");
+    scanf("%d",&ino);
+
+    Display(ino);
+
+    getch();
+    return 0;
+}
